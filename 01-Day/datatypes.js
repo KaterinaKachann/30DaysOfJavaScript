9a135840075a6c48354654a72e4fc2d88d24ff9d
Ekaterina <eka.kachan@gmail.com>
2022-11-22 16:01:29 +0300

    feat: solve task for first day

---
+++ 01-Day/datatypes.js
@@ -0,0 +1,6 @@
+import {  str, bool, und, nul } from "./variable.js";
+
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof und);
+console.log(typeof nul);
---
+++ 01-Day/variable.js
@@ -0,0 +1,35 @@
+// export function str (){
+//     return 'str'
+// }
+
+let str = 'str';
+let bool = true;
+let und = undefined;
+let nul = null;
+
+let value1;
+let velue2;
+let value3;
+let value4;
+
+let valueAss1 = '1';
+let valueAss2 = '2';
+let valueAss3 = '3';
+let valueAss4 = '4';
+
+let firstName = 'Kate';
+let lastName = 'Kachan';
+let maritalStatus = 'single';
+let country = 'Belarus';
+let age = 25;
+
+
+let FirstName = 'Kate'; let LastName = 'Kachan'; let MaritalStatus = 'single'; let Country = 'Belarus'; let Age = 25;
+
+let myAge = `I am ${age} years old`;
+let yourAge = `You are ${Age} years old`;
+
+console.log(myAge);
+console.log(yourAge);
+
+export { str, bool, und, nul };
